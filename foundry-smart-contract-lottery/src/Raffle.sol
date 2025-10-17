9b383bb1198cea3fa4839e19a6f51470be24ce76
azalia <mahsa.abdi2@gmail.com>
2025-10-17 18:23:46 +0330

    improve pickWinner and enterRaffle functions

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -2,15 +2,48 @@
 pragma solidity ^0.8.18;
 
 contract Raffle {
-    uint256 private immutable i_enteranceFee;
+    /**
+     * Errors
+     */
+    error Raffle__SendMoreToEnterRaffle();
 
-    constructor(uint256 enteranceFee) {
+    uint256 private immutable i_enteranceFee;
+    uint256 private immutable i_interval; // @dev The duration of the lottery in seconds
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /**
+     * Events
+     */
+    event RaffleEntered(address indexed player);
+
+    constructor(uint256 enteranceFee, uint256 interval) {
         i_enteranceFee = enteranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {}
+    function enterRaffle() external payable {
+        if (msg.value < i_enteranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
 
-    function pickWinner() public {}
+        s_players.push(payable(msg.sender));
+
+        emit RaffleEntered(msg.sender);
+    }
+
+    // 1. Get a random number
+    // 2. Use random number to pick a player from the array
+    // 3. Be automatically called
+    function pickWinner() external {
+        // Check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+
+        // Get our random number
+    }
 
     /* Getter functions */
     function getEnteranceFee() external view returns (uint256) {
