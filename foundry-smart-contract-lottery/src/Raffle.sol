5053b720af0a6b637c11fdb9879a4e911c2db6c3
azalia <mahsa.abdi2@gmail.com>
2025-10-18 17:56:22 +0330

    improve pick winners function & learn CEI pattern

--- foundry-smart-contract-lottery/src/Raffle.sol
+++ foundry-smart-contract-lottery/src/Raffle.sol
@@ -9,7 +9,20 @@ contract Raffle is VRFConsumerBaseV2Plus {
      * Errors
      */
     error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
+    error Raffle__RaffleNotOpen();
 
+    /**
+     * Type Declaration
+     */
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    /**
+     * State Variables
+     */
     uint256 private immutable I_ENTERANCE_FEE;
     uint256 private immutable I_INTERVAL; // @dev T he duration of the lottery in seconds
     uint256 private immutable I_SUBSCRIPTION_ID;
@@ -20,12 +33,15 @@ contract Raffle is VRFConsumerBaseV2Plus {
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint256 private constant ROLL_IN_PROGRESS = 42;
     uint32 private constant NUM_WORDS = 1;
+    address private sRecentWinner;
+    RaffleState private sRaffleState;
 
     /**
      * Events
      */
     event RaffleEntered(address indexed player);
     event DiceRolled(uint256 indexed requestId, address indexed roller);
+    event WinnerPicked(address indexed winner);
 
     constructor(
         uint256 enteranceFee,
@@ -41,11 +57,16 @@ contract Raffle is VRFConsumerBaseV2Plus {
         I_KEY_HASH = gasLane;
         I_SUBSCRIPTION_ID = subscriptionId;
         I_CALLBACK_GAS_LIMIT = callbackGasLimit;
+        sRaffleState = RaffleState.OPEN;
     }
 
     function enterRaffle() external payable {
         if (msg.value < I_ENTERANCE_FEE) {
             revert Raffle__SendMoreToEnterRaffle();
+        }
+
+        if (sRaffleState != RaffleState.OPEN) {
+            revert Raffle__RaffleNotOpen();
         }
 
         sPlayers.push(payable(msg.sender));
@@ -61,6 +82,8 @@ contract Raffle is VRFConsumerBaseV2Plus {
         if ((block.timestamp - sLastTimeStamp) < I_INTERVAL) {
             revert();
         }
+
+        sRaffleState = RaffleState.CALCULATING;
 
         // Get our random number 2.5
         // 1. Request RNG
@@ -80,12 +103,37 @@ contract Raffle is VRFConsumerBaseV2Plus {
 
         uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
     }
- 
+
+
+    // CEI: Checks, Effects, Interactions Pattern
     function fulfillRandomWords(
         uint256 requestId,
         uint256[] calldata randomWords
     ) internal override {
-        
+        // Checks
+        // conditionals
+
+        // s_players = 10
+        // rng = 12
+        // 12 % 10 = 2
+
+        // Effects (Internal Contract State Changes)
+        uint256 indexOfWinner = randomWords[0] % sPlayers.length;
+        address payable recentWinner = sPlayers[indexOfWinner];
+        sRecentWinner = recentWinner;
+
+        sRaffleState = RaffleState.OPEN;
+        sPlayers = new address payable[](0);
+        sLastTimeStamp = block.timestamp;
+        emit WinnerPicked(sRecentWinner)
+
+        // Interactions (External Contract Interactions)
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+
     }
 
     /* Getter functions */
