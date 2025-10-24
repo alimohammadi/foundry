c4cb29835aaedd421811f731318359973bf75935
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-10-24 15:25:52 +0330

    Create fist manual erc20 token

--- foundry-erc20/src/ManualToken.sol
+++ foundry-erc20/src/ManualToken.sol
@@ -2,5 +2,29 @@
 pragma solidity ^0.8.18;
 
 contract ManualToken {
-    
+    mapping(address => uint256) private sBalances;
+
+    function name() public pure returns (string memory) {
+        return "ManualToken";
+    }
+
+    function totalSupply() public pure returns (uint256) {
+        return 100 ether;
+    }
+
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance) {
+        return sBalances[_owner];
+    }
+
+    function trasfer(address _to, uint256 _amount) public {
+        uint256 previousBalance = balanceOf(msg.sender) + balanceOf(_to);
+        sBalances[msg.sender] -= _amount;
+        sBalances[_to] += _amount;
+
+        require(balanceOf(msg.sender) + balanceOf(_to) == previousBalance);
+    }
 }
