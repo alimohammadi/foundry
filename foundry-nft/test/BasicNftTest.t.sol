55f9e97d051267d81d65897dce383c2392bbc4fb
Ali Mohammadi <alimohammadi02321@gmail.com>
2025-10-25 12:56:44 +0330

    Write test for simple nft

---
+++ foundry-nft/test/BasicNftTest.t.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+import {Test} from "forge-std/Test.sol";
+import {DeployBasicNft} from "../script/DeployBasicNft.s.sol";
+import {BasicNFT} from "../src/BasicNFT.sol";
+
+contract BasicNftTest is Test {
+    DeployBasicNft deployer;
+    BasicNFT public basicNft;
+    address public USER = makeAddr("USER");
+    string public constant PUG =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+
+    function setUp() public {
+        deployer = new DeployBasicNft();
+        basicNft = deployer.run();
+    }
+
+    function testNameIsCorrect() public {
+        string memory expectedName = "BasicNFT";
+        string memory actualName = basicNft.name();
+
+        // In solidity strings are complicated to compare directly, but you can compare their hashes
+        assert(
+            keccak256(abi.encodePacked(expectedName)) ==
+                keccak256(abi.encodePacked(actualName))
+        );
+    }
+
+    function testCanMintAndGetTokenUri() public {
+        vm.prank(USER);
+        basicNft.mintNFT(PUG);
+
+        assert(basicNft.balanceOf(USER) == 1);
+        assert(
+            keccak256(abi.encodePacked(PUG)) ==
+                keccak256(abi.encodePacked(basicNft.tokenURI(0)))
+        );
+    }
+}
